f163a99c05a6f95dcad6528174220f92ed0e526d
ndemont <nastasia@demont.me>
2021-08-17 12:32:37 +0200

    Begin Vector typedef

--- vector.hpp
+++ vector.hpp
@@ -1,17 +1,31 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-template <typename T>
+template <typename T, class Alloc = std::allocator<T>>
 class vector
 {
 	public:
-		vector<T>(void) : _vector(0), _size(0) {} ;
-		vector<T>(size_type size, T value) : _vector(new vector<T>(size)), _size(size)
+		
+		/* MEMBER TYPES */
+		//const_iterator
+		//reverse_iterator
+		//const_reverse_iterator
+		//difference_type
+		typedef T					value_type;
+		typedef alloc 				allocator_type;
+		typedef value_type &		reference;
+		typedef value_type const &	reference;
+		typedef value_type *		pointer;
+		typedef value_type const *	pointer;
+		typedef size_t 				size_type; 
+
+		explicit vector<T>(void) : _vector(0), _size(0) {} ;
+		explicit vector<T>(size_type n, const value_type& val) : _vector(new vector<T>(size)), _size(size)
 		{
 			for (size_type i = 0; i < size; i++) 
-				_vector[i] = value;
+				_vector[i] = val;
 		}
-		vector<T>(vector<T>::iterator begin, vector<T>::iterator end)
+		vector<T>(vector<T>::iterator first, vector<T>::iterator last)
 		{
 			_size = end - begin;
 			_vector = new vector<T>(_size);
@@ -22,7 +36,7 @@ class vector
 				begin++;
 			}
 		}
-		vector<T>(vector<T> const & src) 
+		vector<T>(const vector<T>&x) 
 		{
 			if (this != &src)
 				*this = src;
@@ -42,8 +56,11 @@ class vector
 				_vector[i] = rhs[i];
 			return *this;
 		}
+
+
+
 	private:
-	T			*_vector;
+	value_type			*_vector;
 	size_type	_size;
 };
 

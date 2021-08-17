485681c4aa5707f99f9b8b32e3818c9e1d0c490d
ndemont <nastasia@demont.me>
2021-08-17 16:11:45 +0200

    Add iterators_traits

---
+++ iterator_traits.hpp
@@ -0,0 +1,39 @@
+#ifndef ITERATOR_TRAITS_HPP
+# define ITERATOR_TRAITS_HPP
+
+namespace ft
+
+template <typename Iterator>
+class iterator_traits
+{
+	public:
+		typedef Iterator::difference_type	difference_type;
+		typedef Iterator::value_type		value_type;
+		typedef Iterator::pointer			pointer;
+		typedef Iterator::reference			reference;
+		typedef Iterator::iterator_category	iterator_category;
+};
+
+template <class T>
+class iterator_traits<T*>
+{
+	public:
+		typedef ptrdiff_t					difference_type;
+		typedef T 							value_type;
+		typedef T* 							pointer;
+		typedef T& 							reference;
+		typedef random_access_iterator_tag	iterator_category;
+};
+
+template <class T>
+class iterator_traits<const T*>
+{
+	public:
+		typedef ptrdiff_t					difference_type;
+		typedef T 							value_type;
+		typedef T* 							pointer;
+		typedef T& 							reference;
+		typedef random_access_iterator_tag	iterator_category;
+};
+
+#endif
--- vector.hpp
+++ vector.hpp
@@ -1,39 +1,45 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-template <typename T, class Alloc = std::allocator<T>>
+# include "itertor_traits.hpp"
+
+template <typename T, class Alloc = std::allocator<T> >
 class vector
 {
 	public:
 		
 		/* MEMBER TYPES */
-		//const_iterator
-		//reverse_iterator
-		//const_reverse_iterator
-		//difference_type
-		typedef T					value_type;
-		typedef alloc 				allocator_type;
-		typedef value_type &		reference;
-		typedef value_type const &	reference;
-		typedef value_type *		pointer;
-		typedef value_type const *	pointer;
-		typedef size_t 				size_type; 
+		typedef T											value_type;
+		typedef Alloc										allocator_type;
+		typedef allocator_type::reference					reference;
+		typedef allocator_type::const_reference				const_reference;
+		typedef allocator_type::pointer						pointer;
+		typedef allocator_type::const_pointer				const_pointer;
+		typedef std::random_access_iterator_tag				iterator;
+		typedef cosnt std::random_access_iterator_tag		const_iterator;
+		typedef reverse_iterator<iterator>					reverse_iterator;
+		typedef const reverse_iterator<iterator>			const_reverse_iterator;
+		typedef	iterator_traits<iterator>::difference_type	difference_type;
+		typedef	unsigned difference_type 					size_type;
 
-		explicit vector<T>(void) : _vector(0), _size(0) {} ;
-		explicit vector<T>(size_type n, const value_type& val) : _vector(new vector<T>(size)), _size(size)
+		/* CONSTRUCTORS */
+		explicit vector<T>(const allocator_type& alloc = allocator_type()) : m_alloc(alloc), m_vector(0) {};
+		explicit vector<T>(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 		{
-			for (size_type i = 0; i < size; i++) 
-				_vector[i] = val;
+			for (size_type i = 0; i < n; i++) 
+				m_vector[i] = val;
 		}
-		vector<T>(vector<T>::iterator first, vector<T>::iterator last)
+		template <class iterator<T> >
+        vector (iterator<T> first, iterator<T> last, const allocator_type& alloc = allocator_type());
 		{
-			_size = end - begin;
-			_vector = new vector<T>(_size);
-			for (size_type i = 0; begin != end; i++)
+			if (m_vector)
+				delete m_vector;
+			m_vector = new vector<T>(last - min);
+			for (size_type i = 0; min != last; i++)
 			{
-				_vector[i] = *begin;
+				_vector[i] = *min;
 				i++;
-				begin++;
+				min++;
 			}
 		}
 		vector<T>(const vector<T>&x) 
@@ -49,19 +55,18 @@ class vector
 
 		vector<T> const & operator=(vector<T> const & rhs)
 		{
-			delete _vector;
-			_size = size.rhs;
-			_vector = new vector<T>(_size);
+			if (m_vector)
+				delete m_vector;
+			m_vector = new vector<T>(_size);
 			for (size_type i = 0; i < _size; i++)
-				_vector[i] = rhs[i];
+				m_vector[i] = rhs[i];
 			return *this;
 		}
 
-
-
 	private:
-	value_type			*_vector;
-	size_type	_size;
+	value_type		*m_vector;
+	//size_type		m_size;
+	allocator_type 	m_alloc;
 };
 
 #endif

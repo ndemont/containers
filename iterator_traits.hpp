94b2b1b47c135b8201530f28df863e8f32e35e15
Nastasia Demont <nastasia@demont.me>
2021-09-01 12:58:05 +0200

    Norme iterator_traits.hpp

--- iterator_traits.hpp
+++ iterator_traits.hpp
@@ -9,6 +9,7 @@ namespace ft
 	struct forward_iterator_tag {};
 	struct bidirectional_iterator_tag {};
 	struct random_access_iterator_tag {};
+
 	template <class Iterator>
 	class iterator_traits
 	{

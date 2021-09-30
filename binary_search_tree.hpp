286cfa4ebbed4753d576046e632b3a3f6996f6db
Nastasia Demont <nastasia@demont.me>
2021-09-30 15:21:19 +0200

    Change constructor

--- binary_search_tree.hpp
+++ binary_search_tree.hpp
@@ -19,26 +19,38 @@ class map_iterator;
 template <class T>
 class tree
 {
-	typedef	T										value_type;
-	typedef value_type&								reference;
-	typedef const value_type&						const_reference;
-	typedef value_type*								pointer;
-	typedef const value_type*						const_pointer;
-	typedef ft::map_iterator<value_type>			iterator;
-	typedef ft::const_map_iterator<value_type>		const_iterator;
-	typedef ft::reverse_iterator<iterator>			reverse_iterator;
-	typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
-	typedef ptrdiff_t								difference_type;
-	typedef size_t									size_type;
-
 	public:
+		typedef	T										value_type;
+		typedef value_type&								reference;
+		typedef const value_type&						const_reference;
+		typedef value_type*								pointer;
+		typedef const value_type*						const_pointer;
+		typedef ft::map_iterator<value_type>			iterator;
+		typedef ft::const_map_iterator<value_type>		const_iterator;
+		typedef ft::reverse_iterator<iterator>			reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+		typedef ptrdiff_t								difference_type;
+		typedef size_t									size_type;
+
+		tree(value_type	pr)
+		{
+			pair = &pr;
+			color = BLACK;
+			father = NULL;
+			left = NULL;
+			right = NULL;
+		}
+	
+		tree(const tree& x)
+		{ 
+			(void)x; 
+		}
+
 		value_type		*pair;
 		size_type		color;
 		tree			*father;
 		tree			*left;
 		tree			*right;
-
-	public:
 
 		bool	check_key(tree *root, value_type pair)
 		{

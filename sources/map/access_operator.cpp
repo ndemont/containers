dde43ac3e07bdad30a43fa78cad97a6af8856897
Nastasia DEMONT <ndemont@student.42.fr>
2021-10-26 11:26:14 +0200

    Correct map access operator test

--- sources/map/access_operator.cpp
+++ sources/map/access_operator.cpp
@@ -4,14 +4,14 @@ int	main(void)
 {
 	NAMESPACE::map<char, std::string> mymap;
 
-	mymap['a']="an element";
-	mymap['b']="another element";
-	mymap['c']=mymap['b'];
+	mymap['a'] = "an element";
+	mymap['b'] = "another element";
+	mymap['c'] = mymap['b'];
 
-	std::cout << "mymap['a'] is " << mymap['a'] << '\n';
-	std::cout << "mymap['b'] is " << mymap['b'] << '\n';
-	std::cout << "mymap['c'] is " << mymap['c'] << '\n';
-	std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+	std::cout << "mymap['a'] is " << mymap['a'] << std::endl;
+	std::cout << "mymap['b'] is " << mymap['b'] << std::endl;
+	std::cout << "mymap['c'] is " << mymap['c'] << std::endl;
+	std::cout << "mymap['d'] is " << mymap['d'] << std::endl;
 
 	std::cout << "mymap now contains " << mymap.size() << " elements." << std::endl;
 
